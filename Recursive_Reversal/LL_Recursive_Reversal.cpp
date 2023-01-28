91e7b8a8e8506f4b6122b36e36428667a3d4056e
Armaan Singh Dhaliwal <117513972+Armaan-Dhaliwal@users.noreply.github.com>
2023-01-28 11:58:17 +0530

    Add files via upload

---
+++ Recursive_Reversal/LL_Recursive_Reversal.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "Node_Class.cpp"
+using namespace std;
+
+Node *Insert_Elements()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int data;
+    cout << "Enter your elements: " << endl;
+    cin >> data;
+
+    while (data != -1)
+    {
+
+        Node *newNode = new Node(data);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    cout << "Elements are:" << endl;
+    while (temp != NULL)
+    {
+        cout << temp->data << "\t";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int Length(Node *head)
+{
+    int count = 0;
+
+    while (head->next != NULL)
+    {
+        count++;
+        head = head->next;
+    }
+    count++;
+    return count;
+}
+
+Node *Recursive_Reverse(Node *head, Node *prev)
+{
+
+    if (head->next == NULL)
+    {
+        head->next = prev;
+        return head;
+    }
+
+    Node *next = head->next;
+    head->next = prev;
+    prev = head;
+    return Recursive_Reverse(next, prev);
+}
+
+int main()
+{
+
+    int t;
+    cout << "Enter number of test cases: " << endl;
+    cin >> t;
+    while (t > 0)
+    {
+        t--;
+        Node *head = Insert_Elements();
+        print(head);
+
+        int len = Length(head);
+        Node *prev = NULL;
+        head = Recursive_Reverse(head, prev);
+        cout << "After Reversal ";
+        print(head);
+    }
+}
